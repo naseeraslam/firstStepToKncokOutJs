0ebd4baefdf7c5d4a0186e20aed52475ee7b9efd
MUHAMMAD NASEER ASLAM <naseeraslam456@gmail.com>
2022-08-15 15:04:58 +0500

    Data sharing in mutiple knockout js files

--- app/code/NaseerAslam/KnockoutPractice/view/frontend/web/js/box-configurations.js
+++ app/code/NaseerAslam/KnockoutPractice/view/frontend/web/js/box-configurations.js
@@ -9,43 +9,33 @@
 define(
     [
         'uiComponent',
-        'ko'
+        'ko',
+        'NaseerAslam_KnockoutPractice/js/model/box-configuration'
     ],
     function (
         Component,
-        ko
+        ko,
+        boxConfigurationModel
     ) {
         'use strict';
-        const boxMultiDimensional = ()=> {
-            return {
-                length: ko.observable(),
-                width: ko.observable(),
-                height: ko.observable(),
-                weight: ko.observable(),
-                unitsPerBox: ko.observable(),
-                numberOfBoxes: ko.observable(),
-            };
-        };
+
 
         return Component.extend({
             defaults:{
                 // Best approach to write in xml file so easy override
                 // template: 'NaseerAslam_KnockoutPractice/sku-lookup'
                 boxConfigurations: ko.observableArray(['abc','def ']),
-                //Lets play with multi dimensional array
-                boxMultiDimensional: ko.observableArray([
-                    boxMultiDimensional()
-                ])
+                boxConfigurationModel: boxConfigurationModel
             },
             initialize(){
                 this._super();
                 console.log('Woww its great to practice');
             },
             addnewRow(){
-                this.boxMultiDimensional.push(boxMultiDimensional());
+                boxConfigurationModel.add();
             },
             deleteRow(index){
-                this.boxMultiDimensional.splice(index,1);
+                boxConfigurationModel.delete(index);
             },
             handleSubmit(){
                 console.log('Submitted sucessfully');
